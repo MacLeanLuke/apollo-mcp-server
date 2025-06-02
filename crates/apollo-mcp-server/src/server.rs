056ca5ab984d4357f54c142891391c35084f1c1f
Jeffrey Burt <jeffrey.m.burt@gmail.com>
2025-06-02 16:02:41 -0700

    fetch operation collection

--- crates/apollo-mcp-server/src/server.rs
+++ crates/apollo-mcp-server/src/server.rs
@@ -767,6 +767,9 @@ impl StateMachine {
                 ServerEvent::OperationError(e) => {
                     State::Error(ServerError::Operation(OperationError::File(e)))
                 }
+                ServerEvent::CollectionError(e) => {
+                    State::Error(ServerError::Operation(OperationError::Collection(e)))
+                }
                 ServerEvent::Shutdown => match state {
                     State::Running(running) => {
                         running.cancellation_token.cancel();
